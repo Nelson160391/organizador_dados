72376bbf9e3a55fb18bf2800505abc5c53f60826
Nelson160391 <nelsonjosesilva@gmail.com>
2025-06-09 14:10:05 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,1 +1,8 @@
-print()
+# Criar diretório
+if not os.path.exists('dados'):
+os.mkdir('dados')
+# Criar arquivo com dados
+with open('dados/usuarios.txt', 'w') as arquivo:
+arquivo.write('nome,idade\n')
+arquivo.write('Débora,35\n')
+arquivo.write('Lucas,30\n')
